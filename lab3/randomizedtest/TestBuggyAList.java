654796367e21502376628ebef65474129e4a3b08
sonder037 <whuzjchang@gmail.com>
2025-12-18 18:18:46 +0800

    feat:finish randomized tests

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -8,5 +8,26 @@ import static org.junit.Assert.*;
  * Created by hug.
  */
 public class TestBuggyAList {
-  // YOUR TESTS HERE
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> correct = new AListNoResizing<>();
+        BuggyAList<Integer> broken = new BuggyAList<>();
+        int N = 5000;
+        for (int i = 0; i < N; i += 1) {
+            int opt = StdRandom.uniform(0, 4);
+            if (opt == 0) {
+                int value = StdRandom.uniform(0, 100);
+                correct.addLast(value);
+                broken.addLast(value);
+            } else if (opt == 1) {
+                assertEquals(correct.size(), broken.size());
+            } else if (correct.size() == 0) {
+                continue;
+            } else if (opt == 2) {
+                assertEquals(correct.getLast(), broken.getLast());
+            } else if (opt == 3) {
+                assertEquals(correct.removeLast(), broken.removeLast());
+            }
+        }
+    }
 }
