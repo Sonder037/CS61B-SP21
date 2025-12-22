b7c2c0561043c0595cebd262d90252b904419fa9
Sonder037 <whuzjchang@gmail.com>
2025-12-22 22:21:46 +0800

    fix: fix interface implementation

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -2,7 +2,7 @@ package deque;
 
 import java.util.Iterator;
 
-public class LinkedListDeque<T> implements Deque<T> {
+public class LinkedListDeque<T> implements Deque<T>, Iterable<T> {
     private int size;
     private ListNode<T> head, tail;
 
@@ -10,7 +10,7 @@ public class LinkedListDeque<T> implements Deque<T> {
         private T data;
         private ListNode<T> pre, next;
 
-        public ListNode(T data, ListNode<T> pre, ListNode<T> next) {
+        ListNode(T data, ListNode<T> pre, ListNode<T> next) {
             this.data = data;
             this.pre = pre;
             this.next = next;
